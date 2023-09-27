50a1653f1d590e61861b0804c2658fb23deec7c5
Dave <d.r.young@qub.ac.uk>
2023-09-27 13:52:21 +0100

    x

--- lvk_match_exposures_to_maps.py
+++ lvk_match_exposures_to_maps.py
@@ -69,7 +69,9 @@ def main(arguments=None):
 
     nside = 128
     maps = list_maps_still_to_be_covered(dbConn=dbConn, log=log)
+    print(len(maps))
     for mmap in maps:
+        print(mmaps["id"])
         atExps, psExps = get_exposures_in_maps_temporal_window(log=log, dbConn=dbConn, mmap=mmap, windowDays=7)
         match_exp_to_map_pixels(log=log, dbConn=dbConn, exps=atExps, mapId=mmap["mapId"], survey="atlas", nside=nside, pointingSide=5.46)
         match_exp_to_map_pixels(log=log, dbConn=dbConn, exps=psExps, mapId=mmap["mapId"], survey="ps", nside=nside, pointingSide=0.4)
