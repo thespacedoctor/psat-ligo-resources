7148e28ef4b580716d573df154fed47b7c087340
Dave <d.r.young@qub.ac.uk>
2025-10-03 12:01:40 +0100

    x

--- lvk_map_pixels_to_db.py
+++ lvk_map_pixels_to_db.py
@@ -105,7 +105,7 @@ def list_maps_to_be_imported(
     )
 
     sqlQuery = f"""
-        select primaryId, map from alerts where significant = 1 and primaryId not in (select distinct mapId from alert_pixels_128 where mapId is not null);
+        select primaryId, map from alerts where significant = 1 and primaryId in (select distinct mapId from alert_pixels_128 where mapId is not null);
     """
     maps = readquery(
         log=log,
